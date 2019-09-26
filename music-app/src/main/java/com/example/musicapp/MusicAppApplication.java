f23bd59739272fb880ce89cb6250d38a896edb3b
Janna Pierre <jannapierre@gmail.com>
2019-09-26 17:13:41 -0400

    Initialized new files with maven.

--- music-app/src/main/java/com/example/musicapp/MusicAppApplication.java
+++ music-app/src/main/java/com/example/musicapp/MusicAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MusicAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MusicAppApplication.class, args);
 	}
 
 }
