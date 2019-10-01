6a8b4c508c6a38231e66d51fc7057fe9d8ae33df
Janna Pierre <jannapierre@gmail.com>
2019-10-01 18:43:38 -0400

    Added roles to database manually.Made changes to UserServiceImpl to find role then create new user.

--- music-app/src/main/java/com/example/musicapp/repository/UserRoleRepository.java
+++ music-app/src/main/java/com/example/musicapp/repository/UserRoleRepository.java
@@ -1,8 +1,10 @@
 package com.example.musicapp.repository;
 import com.example.musicapp.models.UserRole;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRoleRepository extends CrudRepository<UserRole, Integer> {
 
+    @Query("FROM UserRole r WHERE r.name = ?1") //
     public UserRole findByName(String name);
 }
