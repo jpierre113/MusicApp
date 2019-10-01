b94bbdeb9159bbe6a831c0abd1084d9eedaebc4e
Janna Pierre <jannapierre@gmail.com>
2019-09-30 20:36:05 -0400

    User signup works.

--- music-app/src/main/java/com/example/musicapp/controller/UserController.java
+++ music-app/src/main/java/com/example/musicapp/controller/UserController.java
@@ -3,10 +3,7 @@ package com.example.musicapp.controller;
 import com.example.musicapp.models.User;
 import com.example.musicapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -21,5 +18,10 @@ public class UserController {
     @PostMapping("/signup")
     public User createUser(@RequestBody User newUser) {
         return userService.createUser(newUser);
+    }
+
+    @GetMapping("/login/{username}/{password}")
+    public User login(@PathVariable String username, @PathVariable String password) {
+        return userService.login(username, password);
     }
 }
--- music-app/src/main/java/com/example/musicapp/service/UserService.java
+++ music-app/src/main/java/com/example/musicapp/service/UserService.java
@@ -7,5 +7,6 @@ public interface UserService {
     public Iterable<User> listUsers();
     public User createUser(User newUser);
     public void deleteById(Long userId);
+    public User login(String username, String password);
 }
 
