6655e67ecff16d64b5430889d6d2abaf23876c8b
Samantha Richards <srichards567@gmail.com>
2019-10-01 15:36:51 -0400

    created song table.

--- music-app/src/main/java/com/example/musicapp/models/Song.java
+++ music-app/src/main/java/com/example/musicapp/models/Song.java
@@ -1,4 +1,62 @@
 package com.example.musicapp.models;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "song")
 public class Song {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String length;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "user_songs",
+            joinColumns = {@JoinColumn(name = "song_id")},
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    public Song() {}
+
+    public List<User> getUsers(){ return users; }
+
+    public void setUsers(List<User> users) { this.users = users; }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
 }
