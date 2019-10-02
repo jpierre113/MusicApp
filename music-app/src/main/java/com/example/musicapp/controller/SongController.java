2234f95f0654fb3fe15be55185f0326fb0f426e9
Samantha Richards <srichards567@gmail.com>
2019-10-02 10:27:42 -0400

    base code for songcontroller.

--- music-app/src/main/java/com/example/musicapp/controller/SongController.java
+++ music-app/src/main/java/com/example/musicapp/controller/SongController.java
@@ -1,4 +1,27 @@
 package com.example.musicapp.controller;
 
+import com.example.musicapp.models.Song;
+import com.example.musicapp.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/song")
 public class SongController {
+
+    @Autowired
+    SongService songService;
+
+    @PostMapping
+    public Song createSong(@RequestBody Song song) {
+        return songService.createSong(song);
+    }
+
+    @GetMapping("/list")
+    public Iterable<Song> listSongs() {
+        return songService.listSongs();
+    }
 }
