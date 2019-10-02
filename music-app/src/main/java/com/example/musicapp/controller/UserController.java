f8cd70b352a35c644c224ef8294dd482f580ad8a
Samantha Richards <srichards567@gmail.com>
2019-10-02 10:59:30 -0400

    added componentscan/component to resolve bean issue.

--- music-app/src/main/java/com/example/musicapp/controller/UserController.java
+++ music-app/src/main/java/com/example/musicapp/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.example.musicapp.controller;
 
+//import com.example.musicapp.model.JwtResponse;
 import com.example.musicapp.models.User;
 import com.example.musicapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
     @Autowired
     private UserService userService;
+
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody User user) {
+//        return ResponseEntity.ok(new JwtResponse(userService.login(user)));
+//    }
 
     @GetMapping("/user/list")
     public Iterable<User> listUsers(){
--- music-app/src/main/java/com/example/musicapp/service/SongService.java
+++ music-app/src/main/java/com/example/musicapp/service/SongService.java
@@ -1,7 +1,9 @@
 package com.example.musicapp.service;
 
 import com.example.musicapp.models.Song;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface SongService {
     public Song createSong(Song song);
 
